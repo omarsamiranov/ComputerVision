512e0cda4ed045012de693a27e467559a8dcfb64
omarsamiranov <132214658+omarsamiranov@users.noreply.github.com>
2023-10-19 22:27:22 +0300

    f

--- VisualOdometry/visual_odometry.py
+++ VisualOdometry/visual_odometry.py
@@ -15,6 +15,7 @@ class VisualOdometry():
         self.K, self.P = self._load_calib(os.path.join(data_dir, 'calib.txt'))
         self.gt_poses = self._load_poses(os.path.join(data_dir,"poses.txt"))
         self.images = self._load_images(os.path.join(data_dir,"images"))
+        self.image_paths = [f"images/{file}" for file in sorted(os.listdir(os.path.join(data_dir,"images")))]
         self.orb = cv2.ORB_create(3000)
         FLANN_INDEX_LSH = 6
         index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6, key_size=12, multi_probe_level=1)
@@ -76,7 +77,6 @@ class VisualOdometry():
         images (list): grayscale images
         """
         image_paths = [os.path.join(filepath, file) for file in sorted(os.listdir(filepath))]
-        self.image_paths = image_paths
         return [cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in image_paths]
 
     @staticmethod
@@ -235,11 +235,91 @@ def sharpness(gray):
 	fm = variance_of_laplacian(gray)
 	return fm
 
+def modify_matrix(up, c2w):
+    c2w[0:3,2] *= -1 # flip the y and z axis
+    c2w[0:3,1] *= -1
+    c2w = c2w[[1,0,2,3],:]
+    c2w[2,:] *= -1 # flip whole world upside down
+
+    up += c2w[0:3,1]
+    return c2w, up
+
+
+def rotmat(a, b):
+	a, b = a / np.linalg.norm(a), b / np.linalg.norm(b)
+	v = np.cross(a, b)
+	c = np.dot(a, b)
+	# handle exception for the opposite direction input
+	if c < -1 + 1e-10:
+		return rotmat(a + np.random.uniform(-1e-2, 1e-2, 3), b)
+	s = np.linalg.norm(v)
+	kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+	return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2 + 1e-10))
+
+def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
+	da = da / np.linalg.norm(da)
+	db = db / np.linalg.norm(db)
+	c = np.cross(da, db)
+	denom = np.linalg.norm(c)**2
+	t = ob - oa
+	ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
+	tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
+	if ta > 0:
+		ta = 0
+	if tb > 0:
+		tb = 0
+	return (oa+ta*da+ob+tb*db) * 0.5, denom
+
+
+def modify_matrices(up, out):
+    up = up / np.linalg.norm(up)
+    print("up vector was", up)
+    R = rotmat(up,[0,0,1]) # rotate up vector to [0,0,1]
+    R = np.pad(R,[0,1])
+    R[-1, -1] = 1
+
+    for f in out["frames"]:
+        f["transform_matrix"] = np.matmul(R, f["transform_matrix"]) # rotate up to be the z axis
+
+    # find a central point they are all looking at
+    print("computing center of attention...")
+    totw = 0.0
+    totp = np.array([0.0, 0.0, 0.0])
+    for f in out["frames"]:
+        mf = f["transform_matrix"][0:3,:]
+        for g in out["frames"]:
+            mg = g["transform_matrix"][0:3,:]
+            p, w = closest_point_2_lines(mf[:,3], mf[:,2], mg[:,3], mg[:,2])
+            if w > 0.00001:
+                totp += p*w
+                totw += w
+    if totw > 0.0:
+        totp /= totw
+    print(totp) # the cameras are looking at totp
+    for f in out["frames"]:
+        f["transform_matrix"][0:3,3] -= totp
+
+    avglen = 0.
+    for f in out["frames"]:
+        avglen += np.linalg.norm(f["transform_matrix"][0:3,3])
+
+    nframes = len(out["frames"])
+    avglen /= nframes
+    print("avg camera distance from origin", avglen)
+    for f in out["frames"]:
+        f["transform_matrix"][0:3,3] *= 4.0 / avglen # scale to "nerf sized"
+    
+    for f in out["frames"]:
+        f["transform_matrix"] = f["transform_matrix"].tolist()
+
+    return out
+
+
 def main():
     data_dir = "video"  # Try KITTI_sequence_2 too
-    os.system(f"rm -r {data_dir}/images")
-    os.system(f"mkdir {data_dir}/images")
-    os.system(f"ffmpeg -i {data_dir}/input.MOV -qscale:v 1 -qmin 1 -vf \"fps=2\" {data_dir}/images/%04d.jpg")
+    # os.system(f"rm -r {data_dir}/images")
+    # os.system(f"mkdir {data_dir}/images")
+    # os.system(f"ffmpeg -i {data_dir}/input.MOV -qscale:v 1 -qmin 1 -vf \"fps=5\" {data_dir}/images/%04d.jpg")
 
     vo = VisualOdometry(data_dir)
 
@@ -249,6 +329,8 @@ def main():
     estimated_path = []
     # for i, gt_pose in enumerate(tqdm(vo.gt_poses, unit="pose")):
     res = []
+
+    up = np.zeros(3)
     for i in range(len(vo.images)):
         try:
             print(i)
@@ -260,6 +342,7 @@ def main():
                 cur_pose = np.matmul(cur_pose, np.linalg.inv(transf))
             # gt_path.append((gt_pose[0, 3], gt_pose[2, 3]))
             estimated_path.append((cur_pose[0, 3], cur_pose[2, 3]))
+            cur_pose, up = modify_matrix(up, cur_pose)
             print(type(cur_pose))
             print(cur_pose)
             print(f"sharpness: {sharpness(vo.images[i])}")
@@ -274,6 +357,8 @@ def main():
           print("#################################33")
 
     res = {"frames": res}
+
+    res = modify_matrices(up, res)
     with open('transforms.json', 'w') as f:
         json.dump(res, f)
     # plotting.visualize_paths(gt_path, estimated_path, "Visual Odometry", file_out=os.path.basename(data_dir) + ".html")
